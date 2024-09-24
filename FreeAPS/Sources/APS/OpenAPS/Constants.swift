d6f5aa47b093c566f39cc818f90f20a8e92711be
Jonas Björkert <jonas@bjorkert.se>
2024-09-24 20:47:02 +0200

    Push notes treatments to Nightscout when Remote Control errors are detected

--- FreeAPS/Sources/APS/OpenAPS/Constants.swift
+++ FreeAPS/Sources/APS/OpenAPS/Constants.swift
@@ -87,6 +87,7 @@ extension OpenAPS {
         static let uploadedProfile = "upload/uploaded-profile.json"
         static let uploadedPreferences = "upload/uploaded-preferences.json"
         static let uploadedSettings = "upload/uploaded-settings.json"
+        static let uploadedNotes = "upload/uploaded-notes.json"
     }
 
     enum FreeAPS {
